71de34e2b247e02e28c50279aafb3fb8747a73b1
sudo-jtcsec <63924277+sudo-jtcsec@users.noreply.github.com>
2020-07-10 21:13:53 -0400

    Update QRADAR_to_MISP.py
    Updated to make it pretty, commented, and with options

--- QRADAR_to_MISP.py
+++ QRADAR_to_MISP.py
@@ -2,11 +2,12 @@ import requests
 import urllib3
 import pymisp
 import json
+import argparse
 from pymisp import MISPEvent, MISPAttribute
 import urllib.parse
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-# Format
+# Format for integration
 #{ 
 #"Reference_Set_Name": name,
 #"Type": ip|md5|url,
@@ -14,6 +15,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 #"Attribute_Type: any
 #}
 
+#Function to create MISP event ***TO DO****
 def create_misp_event(misp,distribution, t_level, analysis, info):
     event_obj = MISPEvent()
     event_obj.distribution = distribution
@@ -23,16 +25,20 @@ def create_misp_event(misp,distribution, t_level, analysis, info):
     event = misp.add_event(event_obj)
     event_id = event.id
 
+#Query QRadar for a reference set. Name must be URL encoded
 def qrad_api_query_RS(set_name,qradip,api_token):
     url = "https://"+qradip+"/api/reference_data/sets/"+set_name
     headers = headers = { "Version": "12.0" , "Accept": "application/json", "SEC": api_token  }
     r = requests.get(url, headers=headers, verify=False)
+    #return Reference set data in JSON form
     return json.loads(r.text)
 
-def misp_init(url,key):
-    misp = pymisp.PyMISP("https://"+url,key,ssl=False,debug=True)
+#Connect to MISP isntance, return MISP object for additional calls
+def misp_init(url,key,qdebug):
+    misp = pymisp.PyMISP("https://"+url,key,ssl=False,debug=qdebug)
     return misp
 
+#parse the integrations file and return them ina  JSON object
 def get_integrations():
     f = open("integrations.json","r")
     f2j = ""
@@ -41,13 +47,20 @@ def get_integrations():
     f.close()
     return json.loads(f2j)
 
+#Send API call to misp to add attributes to event
 def update_misp(misp,mapping,qrad_ip,api_token):
+    #Get plaintext reference set name from integrations list and URL encode it
     toEncode = mapping["Reference_Set_Name"]
     set_name = urllib.parse.quote(toEncode)
+    #Query QRadar for reference set
     api_response = qrad_api_query_RS(set_name,qrad_ip,api_token)
     data_list = api_response["data"]
+    #Query MISP for event to update
     event = misp.get_event(mapping["EventID"])
+    #get all Attributes (aka IOCs ) from MISP event
     attributes = event["Event"]["Attribute"]
+    #Check if data is already in teh event. If yes, pass. If no, add
+    #***TO DO**** first seen/last seen dates
     is_in_misp = []
     for i in attributes:
         is_in_misp.append(i["value"])
@@ -59,9 +72,31 @@ def update_misp(misp,mapping,qrad_ip,api_token):
             attribute.value = d["value"]
             attribute.type = mapping["Attribute"]
             misp.add_attribute(mapping["EventID"],attribute)
+    
+
+def handle_args():
+    parser = argparse.ArgumentParser(description='Export QRadar reference sets to MISP as attributes of an event')
+    parser.add_argument('-l', dest = "list", help = "list all integrations with 'ALL' or specific UID")
+    parser.add_argument('-i', dest = "id",  help = "run all integrations with 'ALL' or specific integration based off of UID")
+    return parser
 
 def main():
-    confile = open("servers.conf","r")
+    parser = handle_args()
+    args = parser.parse_args()
+    if not args.list and not args.id:
+        print("Wrong options. Stop. Run with -h to become enlightened")
+        exit()
+    print("**********************************************************************************")
+    print("*    ____  _____           _              _          __  __ _____  _____ _____   *")
+    print("*   / __ \|  __ \         | |            | |        |  \/  |_   _|/ ____|  __ \  *")
+    print("*  | |  | | |__) |__ _  __| | __ _ _ __  | |_ ___   | \  / | | | | (___ | |__) | *")
+    print("*  | |  | |  _  // _` |/ _` |/ _` | '__| | __/ _ \  | |\/| | | |  \___ \|  ___/  *")
+    print("*  | |__| | | \ \ (_| | (_| | (_| | |    | || (_) | | |  | |_| |_ ____) | |      *")
+    print("*   \___\_\_|  \_\__,_|\__,_|\__,_|_|     \__\___/  |_|  |_|_____|_____/|_|      *")
+    print("*                                                                                *")
+    print("**********************************************************************************\n")
+    #Get appropriate keys from conf file
+    confile = open("qrad.conf","r")
     for line in confile:
         if "QRAD_API_KEY" in line:
             api_token = line.strip().split('=')[1]
@@ -75,14 +110,35 @@ def main():
         if "QRAD_IP" in line:
             qrad_ip = line.strip().split('=')[1]
             print("Got QRadar IP")
-
-    misp = misp_init(misp_ip,misp_key)
-
+    if not api_token and not misp_key and not misp_ip and not qrad_ip:
+        print("Not all of the neccessarry info was recieved. Your config files SUCKS!")
+        exit()
+    else:
+        print("Got all config information required...\n")
+    #load integrations file
     json = get_integrations()
-    print(type(json))
-    for i in json["integrations"]:
-        update_misp(misp,i,qrad_ip,api_token)
-    #misp.get_event(81)
+    #print out existing integrations
+    if args.list:
+        if args.list=="ALL":
+            count = 0
+            for i in json["integrations"]:
+                print("UID: "+str(count)+" "+str(i))
+                count += 1
+        else:
+            print("UID: "+args.list+" "+str(json["integrations"][int(args.list)]))
+
+    misp = misp_init(misp_ip,misp_key, False)
+    #update misp events
+    if args.id:
+        if args.id == "ALL":
+            for i in json["integrations"]:
+                update_misp(misp,i,qrad_ip,api_token)
+        else:
+            update_misp(misp,json["integrations"][int(args.id)],qrad_ip,api_token)
+        print("Update Success")
+    
+    #for i in json["integrations"]:
+    #    update_misp(misp,i,qrad_ip,api_token)
     confile.close()
     
 
